38fc11896469d4f230ad80dea85f4ba8faf91090
alexHooper <alexmohack@gmail.com>
2018-11-20 23:27:34 +0530

    add styling to template

--- static/js/chat.js
+++ static/js/chat.js
@@ -2,11 +2,25 @@ var socket = require('socket.io-client')('http://localhost:5500');
 
 function appendBotMessage(data) {
 	var message = data['text'];
+	$("#result_div").append("<strong>BOTMESSAGE: </strong>" + message + "<br>");
 }
 
 // console log when socket connects to port 5500
 socket.on('connect', function() {
 	console.log('connection established...')
+});
+
+$("#chat-input").keypress(function (e) {
+    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+		var chatInput = $("#chat-input").val();
+		console.log(chatInput);
+		if (chatInput !== '') {
+			socket.emit('user_uttered', {'message': 'hi', 'sender': 'Rasa'});
+		}
+        return false;
+    } else {
+        return true;
+    }
 });
 
 // event when bot utters message
---
+++ static/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+	var mybot = '<div class="chatCont" id="chatCont">'+
+								'<div class="bot_profile">'+
+									'<img src="{% static \'img/bot2.svg\' %}" class="bot_p_img">'+
+									'<div class="close">'+
+										'<i class="fa fa-times" aria-hidden="true"></i>'+
+									'</div>'+
+								'</div><!--bot_profile end-->'+
+								'<div id="result_div" class="resultDiv"></div>'+
+								'<div class="chatForm" id="chat-div">'+
+									'<div class="spinner">'+
+										'<div class="bounce1"></div>'+
+										'<div class="bounce2"></div>'+
+										'<div class="bounce3"></div>'+
+									'</div>'+
+									'<input type="text" id="chat-input" autocomplete="off" placeholder="Try typing here"'+ 'class="form-control bot-txt"/>'+
+								'</div>'+
+							'</div><!--chatCont end-->'+
+
+							'<div class="profile_div">'+
+								'<div class="row">'+
+									'<div class="col-hgt">'+
+										'<img src="{% static \'img/bot2.svg\' %}" class="img-circle img-profile">'+
+									'</div><!--col-hgt end-->'+
+									'<div class="col-hgt">'+
+										'<div class="chat-txt">'+
+											'Chat with us now!'+
+										'</div>'+
+									'</div><!--col-hgt end-->'+
+								'</div><!--row end-->'+
+							'</div><!--profile_div end-->';
+
+	$("mybot").html(mybot);
+
+	// ------------------------------------------ Toggle chatbot -----------------------------------------------
+	$('.profile_div').click(function() {
+		$('.profile_div').toggle();
+		$('.chatCont').toggle();
+		$('.bot_profile').toggle();
+		$('.chatForm').toggle();
+		document.getElementById('chat-input').focus();
+	});
+
+	$('.close').click(function() {
+		$('.profile_div').toggle();
+		$('.chatCont').toggle();
+		$('.bot_profile').toggle();
+		$('.chatForm').toggle();
+	});
+
+	$("#chat-input").keypress(function (e) {
+	    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+			var chatInput = $("#chat-input").val();
+			$("#result_div").append("<strong>USERMESSAGE:</strong> " + chatInput + "<br>")
+			$("#chat-input").val('')
+	        return false;
+	    } else {
+	        return true;
+	    }
+	});
+
+});

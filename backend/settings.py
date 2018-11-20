a3b1a5420c4e676d00ccb4cf8884352252c98088
alexHooper <alexmohack@gmail.com>
2018-11-20 17:29:23 +0530

    add path for templates and static

--- backend/settings.py
+++ backend/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILE_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]

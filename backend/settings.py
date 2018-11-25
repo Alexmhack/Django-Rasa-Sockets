18fa1f3e370b369df5c62833fd366b64a1c1ffd0
alexHooper <alexmohack@gmail.com>
2018-11-25 09:27:43 +0530

    add settings for heroku

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -130,3 +133,7 @@ STATICFILES_DIRS = [
 CORS_ORIGIN_WHITELIST = (
     'localhost:5500',
 )
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())

5a313c0e6afeced6ffedfca3acee571aca7d8b4d
alexHooper <alexmohack@gmail.com>
2019-08-13 21:15:37 +0530

    remove heroku deploy settings

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,4 +139,4 @@ CORS_ORIGIN_WHITELIST = (
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
